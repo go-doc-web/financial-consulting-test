d3259e73cb0ca73eb7d9df90d7559626dc78e667
Oleh Hubskiy <oleh.hubskiy@bndigital.co>
2025-01-31 14:56:49 +0200

    fix: display pop up after load new page

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,15 +1,18 @@
 // Cookie pop-up
-setTimeout(function () {
-  document.querySelector(".cookie-popup").style.display = "block";
-}, 5000);
-
-document
-  .querySelector(".cookie-popup button")
-  .addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("cookieAccepted")) {
     document.querySelector(".cookie-popup").style.display = "none";
-    localStorage.setItem("cookieAccepted", "true");
-  });
+  } else {
+    setTimeout(function () {
+      document.querySelector(".cookie-popup").style.display = "block";
+    }, 5000);
+  }
 
-if (localStorage.getItem("cookieAccepted")) {
-  document.querySelector(".cookie-popup").style.display = "none";
-}
+  const cookiePopupButton = document.querySelector(".cookie-popup button");
+  if (cookiePopupButton) {
+    cookiePopupButton.addEventListener("click", function () {
+      document.querySelector(".cookie-popup").style.display = "none";
+      localStorage.setItem("cookieAccepted", "true");
+    });
+  }
+});
